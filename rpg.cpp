cf143d674c3c27b7b462b4a2e545b7821bcd1bce
Ericer <idea1407a@gmail.com>
2026-08-26 02:27:55 -0500

    Update rpg.cpp
    pasted in the correct code

--- rpg.cpp
+++ rpg.cpp
@@ -1,39 +1,98 @@
+/*Random Password Generator (rpg) - by Ericer Holm
+ascii table https://www.ascii-code.com/
+variable legnth
+start with letter
+include letters, caps, numbers, special characters
+special characters !@#$%^&*()_-+=[]{}\|~`,.<>/?;:'"
+ASCII 33 to 126
+exclude 1, i, , 0 or o because of simularity
+Got ideas on cryptography from https://generate-random.org/passwords/cpp
+*/
+
 #include <iostream>
 #include <string>
 #include <random>
+using namespace std;
 
-std::string generatePassword(int length = 16, bool includeSpecial = true) {
-    const std::string lowercase = "abcdefghijklmnopqrstuvwxyz";
-    const std::string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const std::string numbers = "0123456789";
-    const std::string special = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+int askout(int size) {
+	
+	cout << "\nHow many characters?: ";
+	cin >> size;
+	
+	return size;
+	};
 
-    std::string chars = lowercase + uppercase + numbers;
-    if (includeSpecial) {
-        chars += special;
-    }
+bool asknew(string forward) {
 
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> distrib(0, chars.length() - 1);
+	char ans = 'n';
+	
+	cout << "Your password is: ";
+	cout << forward;
+	
+	cout << "\n\nWould you like to make another password? ";
+	cin >> ans;
+	
+	if (ans == 'y' or ans == 'Y')
+		return true;
+	else
+		return false;
+};
 
-    std::string password;
-    password.reserve(length);
+bool firstchar(char fchar, string letters) {
 
-    for (int i = 0; i < length; ++i) {
-        password += chars[distrib(gen)];
-    }
-
-    return password;
-}
+	bool ans = false;  //assume false to begin with
+	
+	for (int i = 0; i < letters.length(); i++) {
+			if (fchar == letters[i]) 
+				ans = true; //when the character matches with a letter, then true
+	};
+	
+	if (ans)
+		return false;
+	else
+		return true;
+};
 
 int main() {
-    // Generate a 16-character password with special characters
-    std::string password = generatePassword(16, true);
-    std::cout << password << std::endl;  // Example: aB3$xY7!mN2@pQ9&
+	
+	const string uppercase = "ABCDEFGHJKLMNPQRSTUVWXYZ"; //excluded I and O
+	const string lowercase = "abcdefghjklmnpqrstuvwxyz"; //excluded i and o
+	const string numbers = "23456789"; //excluded 1 and 0
+	const string specialchars = "!@#$%^&*()_-+=[]{}|~`,.<>/?;:";
+	const char doublequote = (char)34;
+	const char singlequote = (char)39;
+	const char backslash = (char)92;
+	string alphabet = uppercase + lowercase + specialchars + singlequote + doublequote + backslash;
+	
+	int size = 0;		
+	string pswdstr;
+	
+	cout << "Random Password Generator\n";
+	/* TODO
+	* Make sure the first character is not a special character.
+	*/
+	do { //looping to control the application for the user.
+		//ask how what size is the password
+		size = askout(size);
+		pswdstr.reserve(size);
+		do {
+			pswdstr = "";
+		
+			random_device devicer;
+			mt19937 gen(devicer());
+			uniform_int_distribution<> distrib(0, alphabet.length() - 1);
+			
+			for (int i = 0; i < size; ++i) {
+				pswdstr += alphabet[distrib(gen)];
+			};
+		} while(firstchar(pswdstr[0],uppercase + lowercase)); //if first character of pswdstr is not
+		// a letter, then find a new password.
+	
+	}
+	while(asknew(pswdstr)); //display password and ask if want another
+	
+	}
 
-    // Generate a 12-character password without special characters
-    std::string simplePassword = generatePassword(12, false);
     std::cout << simplePassword << std::endl;  // Example: aB3xY7mN2pQ9
 
     return 0;
